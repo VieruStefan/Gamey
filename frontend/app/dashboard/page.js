cebaeb01662464bb854523ddda1e6f0eb8c1da7d
sv <stefanstef62@gmail.com>
2025-07-24 12:08:24 +0300

    change host of request

--- frontend/app/dashboard/page.js
+++ frontend/app/dashboard/page.js
@@ -1,6 +1,6 @@
 
 export default async function Page() {
-    let data = await fetch('http://master:8090/api/scrape/product');
+    let data = await fetch('http://service-master:8090/api/scrape/product');
     let posts = await data.json()
     return (
         <ul>
