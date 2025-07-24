7d8751de9acfdeb989525df9600ad88a08680e1a
sv <stefanstef62@gmail.com>
2025-07-24 13:01:31 +0300

    fix api call

--- frontend/app/dashboard/page.js
+++ frontend/app/dashboard/page.js
@@ -1,11 +1,11 @@
 
 export default async function Page() {
-    let data = await fetch('http://172.22.0.11:8090/api/scrape/product');
+    let data = await fetch('http://172.22.0.11:8090/api/diffbot/product');
     let posts = await data.json()
     return (
         <ul>
             {posts.map((post) => (
-                <li key={post.id}>{post.title}</li>
+                <p>{post}</p>
             ))}
         </ul>
     )
